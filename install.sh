ccdac019bc49c00b27d36b1730a6592e5bb4ab8c
Miguel Ccantuta <mccantuta@Miguels-MacBook-Pro.local>
2023-01-12 22:05:45 -0500

    Installation script

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo Installing NeoVim
+echo -----------------
+#brew install nvim
+
+echo Installing Oh My Zsh
+echo --------------------
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
