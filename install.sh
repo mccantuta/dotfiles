00a06129a4137b5f298d66361ec9faacb218afcb
Miguel Ccantuta <mccantuta@yahoo.com>
2023-01-13 13:04:36 -0500

    Update installer

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+echo Installing NodeJS
+echo -----------------
+#brew install node
+
+echo Installing Inconsolata Font
+echo ---------------------
+#brew install font-inconsolata
+
 echo Installing NeoVim
 echo -----------------
 #brew install nvim
@@ -5,3 +13,12 @@ echo -----------------
 echo Installing Oh My Zsh
 echo --------------------
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo Copying Kitty configuration
+echo ---------------------------
+# TODO
+
+echo Copying NeoVim configuration
+echo ----------------------------
+#mkdir ~/.config/nvim
+#cp init.lua ~/.config/nvim/
