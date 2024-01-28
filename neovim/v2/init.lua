b46ccfb346638f6febebcc0b558cfa439911bcb7
Miguel Ccantuta <mccantuta@yahoo.com>
2024-01-28 17:43:45 -0500

    Update Nvim configuration

---
+++ neovim/v2/init.lua
@@ -0,0 +1,14 @@
+require('keymaps')
+require('plugins.lazy')
+require('plugins.misc')
+require('plugins.lualine')
+require('options')
+require('misc')
+require('plugins.dap')
+require('plugins.gitsigns')
+require('plugins.telescope')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.trouble')
+
+--vim: ts=4 sts=2 sw=2 et
---
+++ neovim/v2/lua/plugins/lazy.lua
@@ -0,0 +1,128 @@
+-- Install lazylazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Fixes Notify opacity issues
+--vim.o.termguicolors = true
+
+require('lazy').setup({
+  'onsails/lspkind.nvim',
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+
+  -- Tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    requires = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+  'xiyaowong/nvim-transparent',
+  { 
+    'numToStr/FTerm.nvim',
+    config = function()
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+    require 'FTerm'.setup({
+      blend = 5,
+      dimensions = {
+        height = 0.90,
+        width = 0.90,
+        x = 0.5,
+        y = 0.5
+      }
+    })
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        background_colour = "#000000",
+        enabled = false,
+      })
+    end
+  },   
+'ray-x/go.nvim',
+  {
+    "windwp/nvim-autopairs",
+      config = function() require("nvim-autopairs").setup {} end
+  },
+
+  { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      'j-hui/fidget.nvim',
+    }
+  },
+
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    }
+  },
+
+  { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"} },
+  'theHamsta/nvim-dap-virtual-text',
+  'leoluz/nvim-dap-go',
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'lewis6991/gitsigns.nvim',
+
+  'nvim-lualine/lualine.nvim', -- Fancier statusline
+  'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+  'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines 
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- Fuzzy Finder (files, lsp, etc)
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  'nvim-telescope/telescope-symbols.nvim',
+ 
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
+})
