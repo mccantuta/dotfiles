869ff4da97705ef6134084ca058b313fb1e731e3
Miguel Ccantuta <mccantuta@yahoo.com>
2024-02-25 22:45:08 -0500

    Update wezterm font

--- wezterm/mccantuta/font.lua
+++ wezterm/mccantuta/font.lua
@@ -1,10 +1,13 @@
 local module = {}
 
 function module.apply_font(config, wezterm)
-  --config.font = wezterm.font 'MonaspiceNe Nerd Font Mono'
-  --config.font = wezterm.font 'Inconsolata Nerd Font Mono'
-  config.font = wezterm.font({family = 'JetBrainsMono Nerd Font Mono', weight = 'Bold', style='Normal'})
-  --config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', style="Normal"})
+  config.font = wezterm.font({family = 'MonaspiceNe Nerd Font Mono', weight = 'Regular', style='Normal'})
+  --config.font = wezterm.font({family = 'Inconsolata Nerd Font Mono', weight = 'Regular', style='Normal'})
+  --config.font = wezterm.font({family = 'JetBrainsMono Nerd Font Mono', weight = 'DemiBold', style='Normal'})
+  --config.font = wezterm.font({family = 'Hack Nerd Font Mono', weight = 'Regular', style='Normal'})
+  --config.font = wezterm.font({family = 'Maple Mono', weight = 'Regular', style='Normal'})
+  --config.font = wezterm.font({family = 'JuliaMono', weight = 'Regular', style='Normal'})
+  --config.font = wezterm.font({family = 'FiraMono Nerd Font Mono', weight = 'Regular', style='Normal'})
   config.font_size = 20.0
 end
 
