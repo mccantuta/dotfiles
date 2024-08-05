161ce12b7070c4752c3f9b9e79e06df4dde6ae8e
Miguel Ccantuta <mccantuta@yahoo.com>
2024-08-05 11:05:16 -0500

    Update configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,19 @@
+# README
+# Copy configurarion file to ~/.config/fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 alias l 'ls -la'
 
+# Set environment variables
+# set -x VARIABLE_NAME variable_value
+
+# Add Golang packages to Path
+#fish_add_path /Users/mccantuta/go/bin
+
 starship init fish | source
+
+# Set up fzf key bindings
+# fzf --fish | source
---
+++ install.v2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo Installing software
+echo *******************
+
+echo Installing Homebrew
+echo -------------------
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo Installing terminal dependencies
+echo -----------------
+brew install imagemagick
+brew install node
+brew install luarocks
+brew install cmake
+brew install rg
+
+echo Installing Inconsolata Font
+echo ---------------------
+brew tap homebrew/cask-fonts
+brew install font-inconsolata-nerd-font
+
+echo Installing JetBrains Mono Font
+echo ---------------------
+brew install font-jetbrains-mono-nerd-font
+
+echo Installing Monaspice Font
+echo ---------------------
+brew install font-monaspace-nerd-font
+
+echo Installing Meslo LG Font
+echo ---------------------
+brew install font-meslo-lg-nerd-font
+
+echo Installing Fish Shell
+echo ---------------------
+brew install fish
+which fish
+sudo sh -c 'echo /usr/local/bin/fish >> /etc/shells'
+chsh -s /usr/local/bin/fish
+
+echo Installing NeoVim
+echo -----------------
+brew install nvim
+
+echo Installing Alacritty terminal
+echo -------------------------
+brew install alacritty
+
+echo Setting configuration
+echo *********************
+
+echo Copying NeoVim configuration
+echo ----------------------------
+mkdir ~/.config/nvim
+cp -r neovim/* ~/.config/nvim/
