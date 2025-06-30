730df8e9fd659f55e3a33757558e1ecfc85a5e0f
Miguel Ccantuta <mccantuta@yahoo.com>
2025-06-29 22:52:27 -0500

    Update Golang config

--- neovim/init.lua
+++ neovim/init.lua
@@ -18,6 +18,7 @@ require('plugins.lsp')
 -- Set colorscheme
 --require("themes.vague")
 --require("themes.kanagawa")
-require("themes.gruvbox-material")
+require("themes.gruvbox-material") -- NIGHT
+--require("themes.catppuccin") --DAY
 
 --vim: ts=4 sts=2 sw=2 et
